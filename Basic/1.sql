37149355f2958824d98da07bf6cd0baa03169b41
VIJAY RAJESH R <vijayrajeshr@gmail.com>
2025-05-04 09:07:18 +0530

    file-1-complete

--- Basic/1.sql
+++ Basic/1.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE students;
+USE students;
+
+CREATE TABLE student_info(reg_no VARCHAR(10),name VARCHAR(20),age INT(2),batch YEAR) 
+
+INSERT INTO student_info(reg_no,name,age,batch) VALUES ("23BAI10917","VIJAY RAJESH R",19,2023)
+
+SELECT * FROM student_info;
 
